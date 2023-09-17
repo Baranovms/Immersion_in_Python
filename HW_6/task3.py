af95a3c3e9378bf1ec61460f69c1458fabcd2480
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-09-17 20:44:01 +0300

    add HW_6

--- HW_6/task3.py
+++ HW_6/task3.py
@@ -1,8 +1,4 @@
-def n_queens(board,col, N)
-
-
-
-def safe(board,row,col,N):
+def safe(board,row,col,n):
     for i in range(col):
         if board[[row][i]] == 1:
             return False
@@ -12,4 +8,30 @@ def safe(board,row,col,N):
             return False
         row += 1
         col -= 1
+
+    while row < n and col >= 0:
+        if board[[row][col]] == 1:
+            return False
+        row += 1
+        col -= 1
     
+    
+def n_queens(board,col, n):
+    if col >=N:
+        return True
+    
+    for i in range(n):
+        if safe(board, i, col, n):
+            board[i][col] = 1
+            if n_queens(board, col+1, n):
+                return True
+            board[i][col] = 0
+    return False
+
+def main():
+    board = [[0] * n for _ in range(n)]
+    pass    
+
+n = 8
+if __name__ == "__main__":
+    main()
